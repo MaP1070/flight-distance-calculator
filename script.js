953bde226f8a5c91f5cf8f3eb054950caf4a8dab
MaP1070 <marceloponcepinasco@gmail.com>
2026-03-11 19:13:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-
-function calculate() {
-
-//Def. route
-let from = document.getElementById("from").value.replace(/\s+/g,"").toUpperCase()
-let to = document.getElementById("to").value.replace(/\s+/g,"").toUpperCase()
-
 //Def. airports
 let airports = {};
 
@@ -13,6 +6,17 @@ fetch("airports.json")
 .then(data => {
     airports = data;
 });
+
+function calculate() {
+
+if (Object.keys(airports).length === 0) {
+    alert("Airport database still loading, try again in a moment.");
+    return;
+}
+
+//Def. route
+let from = document.getElementById("from").value.replace(/\s+/g,"").toUpperCase()
+let to = document.getElementById("to").value.replace(/\s+/g,"").toUpperCase()
 
 //Def. origin & destination
  let origin = airports[from]
@@ -54,6 +58,7 @@ document.getElementById("Distance").innerText = message2;
 document.getElementById("Time").innerText = message3;
 document.getElementById("container").style.display = "block";
 }
+
 
 
 
